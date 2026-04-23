b0815255d15610f2bed83a1116268b84ead61ee9
markjones22347-ops <markjones22347@gmail.com>
2026-04-23 06:24:39 -0700

    Add ShowWindow and bring-to-front after WebView2 init

--- imgui_ui/src/main.cpp
+++ imgui_ui/src/main.cpp
@@ -620,6 +620,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
                                     SendToWebView(initMsg.dump());
                                     
                                     g_webviewController->put_IsVisible(TRUE);
+                                    
+                                    // Show the main window after WebView2 is ready
+                                    ShowWindow(g_hMainWindow, SW_SHOW);
+                                    UpdateWindow(g_hMainWindow);
+                                    
+                                    // Bring window to front
+                                    SetForegroundWindow(g_hMainWindow);
+                                    SetFocus(g_hMainWindow);
                                 } else {
                                     MessageBoxA(g_hMainWindow, "Failed to create WebView2 controller", "Error", MB_OK | MB_ICONERROR);
                                 }
